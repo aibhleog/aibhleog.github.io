9c2d040264e764be31104511df0abd3d21abb991
aibhleog <aibhleog@tamu.edu>
2021-10-03 17:10:55 -0500

    finally doing the smart thing :see_no_evil:

---
+++ includes/navigate.js
@@ -0,0 +1,10 @@
+document.write("<ul class='links'>");
+document.write("<li><a href='index.html'>Home</a></li>");
+document.write("<li><a href='aboutme.html'>About Me</a></li>");
+document.write("<li><a href='research.html'>Research</a></li>");
+document.write("<li><a href='github.html'>Github Projects</a></li>");
+document.write("<li><a href='cv.html'>CV &amp; Highlights</a></li>");
+document.write("<li><a href='nsf.html'>NSF GRFP Advice</a></li>");
+document.write("<li><a href='photo-gallery/index.html'>Photo Gallery</a></li>");
+document.write("<li><a href='useful.html'>Resources</a></li>");
+document.write("</ul>");

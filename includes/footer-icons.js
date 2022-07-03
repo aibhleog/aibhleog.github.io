ebdf98ea873d38634f2203b872ea10c7e3f8ff0a
aibhleog <aibhleog@tamu.edu>
2022-07-02 20:08:28 -0500

    long-awaited updates yay :joy:

---
+++ includes/footer-icons.js
@@ -0,0 +1,10 @@
+document.write("<ul class='icons'>");
+document.write("<li><a href='https://github.com/aibhleog' class='icon alt fa-github'><span class='label'>GitHub</span></a></li>");
+document.write("<li><a href='https://twitter.com/aibhleog' class='icon alt fa-twitter'><span class='label'>Twitter</span></a></li>");
+document.write("<li><a href='https://www.instagram.com/gingersnapmadcap/' class='icon alt fa-instagram'><span class='label'>Instagram</span></a></li>");
+document.write("</ul>");
+document.write("<ul class='copyright'>");
+document.write("<li><strong><font color='#589FCE'>aibhleog@tamu.edu</font></strong></li>");
+document.write("<li>&copy; Taylor Hutchison</li>");
+document.wirte("<li>Design: <a href='https://html5up.net'>HTML5 UP</a></li>");
+//document.write("</ul>");

6abdfb46d664e38e5d54515045b0b3795b8c40a0
aibhleog <aibhleog@tamu.edu>
2022-08-23 22:28:03 -0400

    updating email & job :fire::tada:

---
+++ includes/email.js
@@ -0,0 +1,3 @@
+// made this so it would be easier to change the email when I move places
+// however, still have to manually change the email in footer.js
+document.write("astro.hutchison AT gmail.com");
--- includes/footer-icons.js
+++ includes/footer-icons.js
@@ -4,7 +4,7 @@ document.write("<li><a href='https://twitter.com/aibhleog' class='icon alt fa-tw
 document.write("<li><a href='https://www.instagram.com/gingersnapmadcap/' class='icon alt fa-instagram'><span class='label'>Instagram</span></a></li>");
 document.write("</ul>");
 document.write("<ul class='copyright'>");
-document.write("<li><strong><font color='#589FCE'>aibhleog@tamu.edu</font></strong></li>");
+document.write("<li><strong><font color='#589FCE'>astro.hutchison AT gmail.com</font></strong></li>");
 document.write("<li>&copy; Taylor Hutchison</li>");
 document.wirte("<li>Design: <a href='https://html5up.net'>HTML5 UP</a></li>");
 //document.write("</ul>");

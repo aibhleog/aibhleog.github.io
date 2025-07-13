4b04a595605e41d7c8128127bf9f0a9e24a7c4ed
aibhleog <astro.hutchison@gmail.com>
2025-07-13 12:43:24 +0200

    updated header as includes

---
+++ includes/header_name.js
@@ -0,0 +1,3 @@
+// made this so it would be easier to mess with my name :)
+document.write("<a href='index.html' class='logo'><strong>Taylor</strong> <span>Hutchison</span> &#127817;</a>");
+// added watermelon for Gaza & Palestine, July 2025

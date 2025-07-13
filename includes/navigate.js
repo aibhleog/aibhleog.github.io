63561d081107367e21c5fe4658e5608913ca854d
aibhleog <astro.hutchison@gmail.com>
2025-07-13 12:43:03 +0200

    updating header :watermelon:

--- includes/navigate.js
+++ includes/navigate.js
@@ -2,6 +2,7 @@
 document.write("<h1 class='strongTaylor'><strong><a href='index.html'>Taylor</a></strong></h1>");
 
 document.write("<ul class='links'>");
+document.write("<li><span style='font-size:40px'>&#127817;</span></li>");
 document.write("<li><a href='home.html'>Home</a></li>");
 document.write("<li><a href='aboutme.html'>About Me</a></li>");
 document.write("<li><a href='research.html'>Research</a></li>");
@@ -9,6 +10,11 @@ document.write("<li><a href='github.html'>Github Projects</a></li>");
 document.write("<li><a href='cv.html'>CV &amp; Highlights</a></li>");
 document.write("<li><a href='photo-gallery/index.html'>Photo Gallery</a></li>");
 document.write("<li><a href='useful.html'>Resources</a></li>");
-//document.write("<li><a href='nsf.html'>&mdash; NSF GRFP Advice</a></li>");
-//document.write("<li><a href='job-cycle.html'>&mdash; Job Cycle Guide</a></li>");
+// SPACING
+document.write("<li>.</li>");
+document.write("<li>.</li>");
+document.write("<li>.</li>");
+// SPECIFIC HIGHLIGHTS
+document.write("<li><a href='nsf.html' class='button primary fit' style='background-color:#3f759a;color:#fff'>NSF GRFP Advice</a></li>");
+document.write("<li><a href='job-cycle.html' class='button primary fit' style='background-color:#CD5C5C; color:#fff'>Job Cycle Guide</a></li>");
 document.write("</ul>");
